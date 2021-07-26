6ac2b068217fec78bdc0dec9fe84b3489a81dff0
Saiveetech01 <saiveetech01@gmail.com>
2021-07-26 12:25:55 +0530

    [Saivee][4] Retrieval of customer information

---
+++ SIPL-Excercise-01/phase-4.py
@@ -0,0 +1,9 @@
+def main():
+    file=open('customer-data.txt','rt')
+    file.read()
+    cust_name=input("Enter customer name:")
+    for line in file:
+        for cust_name in line:
+            print(Amount)
+main()    
+    
