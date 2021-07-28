f3d74215fa9ddfc3e5eaee04297179f494fd89b5
Saiveetech01 <saiveetech01@gmail.com>
2021-07-28 18:10:08 +0530

    [Saivee][3] Ramu-Kaka NoteBook

--- SIPL-Excercise-01/phase-4.py
+++
@@ -1,9 +0,0 @@
-def main():
-    file=open('customer-data.txt','rt')
-    file.read()
-    cust_name=input("Enter customer name:")
-    for line in file:
-        for cust_name in line:
-            print(Amount)
-main()    
-    
