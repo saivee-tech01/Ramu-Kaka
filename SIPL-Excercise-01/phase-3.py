158151c15586e24714013d649e493da7892100ca
Saiveetech01 <saiveetech01@gmail.com>
2021-08-02 17:47:05 +0530

    [Saivee][4] Retrieval of information using json

--- SIPL-Excercise-01/phase-3.py
+++ SIPL-Excercise-01/phase-3.py
@@ -1,20 +1,16 @@
 import json
-
 n=int(input("Enter value:"))
 customer={}
-
 for i in range(n):
     name=input("Enter name:")
     data=[]
     amount=int(input("Enter amount:"))
     date=input("Enter date:")
     list=[amount,date]
+    
     data.append(list)
     customer[name]=data
-    
-    
-
-
-
+  
 with open('customer.json','a+') as f:
     j=json.dump(customer,f)
+    
