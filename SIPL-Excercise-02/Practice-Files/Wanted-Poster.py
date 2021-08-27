a6e82e9ae643ebd3ec9936540cc5ea8d4721066b
Saiveetech01 <saiveetech01@gmail.com>
2021-08-27 15:02:20 +0530

    Created Wanted Poster using guizero

--- SIPL-Excercise-02/Practice-Files/Wanted-Poster.py
+++ SIPL-Excercise-02/Practice-Files/Wanted-Poster.py
@@ -1,8 +1,12 @@
 from guizero import *
 
+# Container
 app=App("Wanted!",bg="#228185")
 
+# Text
 wanted_text = Text(app, text="WANTED!!!",size=50)
+
+# Picture
 wanted_pic=Picture(app,image="cat.png")
 
 app.display()
